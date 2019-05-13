345d8953b6031dd81fb6cbfd874ac97a6e3184a8
Dżastin <mc.parafiniuk@uw.edu.pl>
2019-05-13 13:21:03 +0200

    0.2 rotated

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/disintegration/imaging"
 	"github.com/go-ini/ini"
-	"github.com/nfnt/resize"
 
 	_ "image/jpeg"
 )
@@ -29,30 +29,30 @@ func detonate(err error) {
 func main() {
 	cfg, err := ini.Load("wm.ini")
 	detonate(err)
-	WM_COUNTS, err := cfg.Section("").Key("num_wms").Int()
+	WM_OFFSET, err := cfg.Section("").Key("wm_offset").Int()
 	detonate(err)
 	OPACITY, err := cfg.Section("").Key("opacity").Float64()
 	detonate(err)
-	targetFile, err := os.Open(os.Args[1])
+	ANGLE, err := cfg.Section("").Key("angle").Float64()
 	detonate(err)
-	wmFile, err := os.Open("wm.png")
+
+	targetImg, err := imaging.Open(os.Args[1])
 	detonate(err)
-	targetImg, _, err := image.Decode(targetFile)
+	wmImg, err := imaging.Open("wm.png")
 	detonate(err)
-	wmImg, _, err := image.Decode(wmFile)
-	detonate(err)
+
 	rgba := image.NewRGBA(targetImg.Bounds())
 
 	mask := image.NewUniform(color.Alpha{uint8(255 * OPACITY)})
 
-	resizedWm := resize.Resize(uint(targetImg.Bounds().Max.X), 0, wmImg, resize.Lanczos3)
+	rotatedWm := imaging.Rotate(wmImg, ANGLE, color.Transparent)
+
+	resizedWm := imaging.Resize(rotatedWm, targetImg.Bounds().Max.X, 0, imaging.Lanczos)
 
 	draw.Draw(rgba, targetImg.Bounds(), targetImg, image.Point{0, 0}, draw.Src)
 
-	wmYDt := (targetImg.Bounds().Max.Y - resizedWm.Bounds().Max.Y) / (WM_COUNTS - 1)
-
-	for i := 0; i < WM_COUNTS; i++ {
-		draw.DrawMask(rgba, image.Rectangle{image.Point{0, wmYDt * i}, targetImg.Bounds().Max}, resizedWm, image.Point{0, 0}, mask, image.Point{0, 0}, draw.Over)
+	for i := -resizedWm.Bounds().Max.Y; i < targetImg.Bounds().Max.Y/WM_OFFSET; i++ {
+		draw.DrawMask(rgba, image.Rectangle{image.Point{0, WM_OFFSET * i}, targetImg.Bounds().Max}, resizedWm, image.Point{0, 0}, mask, image.Point{0, 0}, draw.Over)
 	}
 
 	extension := filepath.Ext(os.Args[1])
